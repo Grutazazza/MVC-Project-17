d06a0c348233a0a875b8b500f7af8970f04ae95a
is41-plechevdd <steammapps@mail.ru>
2022-03-17 15:56:36 +0500

    Version 1.0.5

--- views/header.php
+++ views/header.php
@@ -7,8 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= (isset($title) ? $title : 'Главная страница') ?></title>
     <?php
-    $css[] = 'bootstrap.css';
-    $javascript[]=['src'=>'bootstrap.js'];
+        $css[] = 'bootstrap.css';
+        $javascript[] = ['src' => 'bootstrap.js'];
+
         if(isset($css))
             foreach($css as $item)
                 echo "<link rel='stylesheet' href='/assets/css/{$item}'>";
@@ -20,11 +21,10 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Мой блок</a>
-        <button class="navbar-toggler" type="button"
-                data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">Мой блог</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -35,13 +35,10 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/login">Авторизация</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="/register">
-                        Регистрация
-                    </a>
+                <li class="nav-item">
+                    <a class="nav-link" href="/register">Регистрация</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
-</body>
