ee9973f0e29d36161fb45faf9814b031ef3196c0
is41-plechevdd <steammapps@mail.ru>
2022-03-17 17:18:25 +0500

    Version 1.0.6 - BUG

--- core/view.php
+++ core/view.php
@@ -1,6 +1,38 @@
 <?php
 function view($name, $arg = []) {
     ob_start();
+    //создание переменной error для записей ошибок
+    $error = ($arg['errors'] ?? []);
+    // Функция в переменной для определения есть ли ошибка
+    $isError = function ($errorName, $input = false) use ($error) {
+        $firstError = $error[$errorName] ?? null;
+        if(!isset($firstError)&&!$input) return '';
+        if($input){
+            $class = (isset($firstError)? 'is-invalid' : '');
+            $arrAttributes =[];
+            $arrAttributes["class"][] = "form-control";
+            $arrAttributes["class"][] = $class;
+            $arrAttributes["aria_describedby"][] = "validation${errorName}Feedback";
+
+            $attributes = '';
+
+            foreach ($arrAttributes as $key => $items){
+                $key = str_replace('_','-',$key);
+                $attributes .=" $key='";
+                foreach ($items as $item)
+                    $attributes.=" $item";
+                $attributes.="'";
+            }
+
+            return $attributes;
+        }
+        else{
+            return '<div id="'."validation{$errorName}".'" class="invalid-feedback">
+                        ' .(isset($firstError)? implode('<br>',$error[$errorName]):'').';
+                    </div>';
+        }
+        return isset($error[$errorName]);
+    };
     include_once 'views/' . $name . '.php';
     $content = ob_get_contents();
     ob_clean();
