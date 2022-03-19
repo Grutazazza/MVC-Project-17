88b0a0bfbceb68cc8b8a265d1ebe7a656e9ba1ee
is41-plechevdd <steammapps@mail.ru>
2022-03-19 15:05:31 +0500

    Version 1.0.7.1

---
+++ core/basefunction.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *Располагается базовая функции доступна ото всюду
+ */
+function session($name){
+    return $_SESSION[$name];
+}
+function has_session($name){
+    return isset($_SESSION[$name]);
+}
+function put_session($name,$value){
+    $_SESSION[$name] = $value;
+}
